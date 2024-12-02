cdec283c5a5c59fb9befa15c042d92fa79c32979
Md Rejaul Karim <rktuhinbd@gmail.com>
2024-12-02 11:56:43 +0600

    Initial Commit

---
+++ First iOS Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  First iOS Project
+//
+//  Created by Md Rejaul Karim on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ First iOS Project/First_iOS_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_iOS_ProjectApp.swift
+//  First iOS Project
+//
+//  Created by Md Rejaul Karim on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct First_iOS_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
